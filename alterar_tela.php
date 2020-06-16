5ab4969230254180feb45927648580b65732389f
Breno Vinicius <>
2020-06-16 00:47:26 -0300

    Commit do Breno Vinicius

--- alterar_tela.php
+++ alterar_tela.php
@@ -1,7 +1,7 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<title>Excluir Conta</title>
+		<title>Alterar</title>
                 
                 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
